e96e45b70ae652f0b60ef73663aa0b28c94ae381
Michael Passow <passowmichael@yahoo.com>
2019-10-08 16:24:58 -0400

    created mongo/mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 
@@ -19,6 +20,17 @@ app.set("view-engine", "handlebars");
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function(error){
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("Mongoose connection is successful!");
+    };
+});
 
 app.listen(PORT, function() {
     console.log("Listening on port: " + PORT);
