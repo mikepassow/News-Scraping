c51618d7fd92e69b1ed9da23a4cbfe47ca3368b7
Michael Passow <passowmichael@yahoo.com>
2019-10-10 16:43:00 -0400

    added all routes

--- config/routes.js
+++ config/routes.js
@@ -35,5 +35,37 @@ router.get("/api/headlines", function(req, res){
         res.json(data);
     });
 });
-
+    router.delete("/api/headlines/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(err,data){
+            res.json(data);
+        });
+    });
+    router.patch("/api/headlines", function(req,res){
+        headlinesController.update(req.body, function(err,data){
+            res.json(data);
+        });
+    });
+    router.get("/api/notes/:headline_id?", function(req, res){
+        var query = {};
+        if (req.params.headline_id) {
+            query._id = req.params.headline_id;
+        }
+        notesController.get(query, function(err, data){
+            res.json(data);
+        });
+    });
+    router.delete("/api/notes/:id", function(req,res){
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function(err, data){
+            res.json(data);
+        });
+    });
+    router.post("/api/notes", function(req, res){
+        notesController.save(req.body, function(data){
+            res.json(data);
+        });
+    });
 }
